6527b11e582cc444c8b967d40ea5bde8e4a79256
rojae1339 <wjdwogh1224@gmail.com>
2025-09-11 11:45:06 +0200

    add

---
+++ Source/FpsWave/FpsWave.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FpsWave.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FpsWave, "FpsWave" );
